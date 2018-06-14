4641098c4475733f69dd98e53070bad76af35bda
ShobhitAnand26 <38216265+ShobhitAnand26@users.noreply.github.com>
2018-06-14 12:18:21 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int t;
+	cin >> t;
+	while(t--)
+    {int n;
+    int x;
+    int i=1;
+     cin >> n;
+     for(x=0;x<=n;i++)
+     {
+         x=x+i;
+     }
+     cout<< i-2<<endl;
+    }
+
+
+
+	return 0;
+
+}
